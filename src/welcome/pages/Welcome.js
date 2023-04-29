00ef84634151d1c966411f00fb51e0e801e4f18a
Jordan Dickson <jfdickson25@gmail.com>
2023-04-29 09:22:00 -0600

    Update to have swipable welcome screen

--- src/welcome/pages/Welcome.js
+++ src/welcome/pages/Welcome.js
@@ -2,10 +2,11 @@ import React, { useState } from 'react';
 import { useHistory } from 'react-router-dom/cjs/react-router-dom.min';
 import Button from '../../shared/components/FormElements/Button';
 
+import { useSwipeable } from 'react-swipeable';
+
 import movieNight from '../assets/img/movie-night.svg';
 import watch from '../assets/img/watch.svg';
 import './Welcome.css';
-
 const Welcome = props => {
     const history = useHistory();
     const [isWelcome, setIsWelcome] = useState(true);
@@ -21,6 +22,26 @@ const Welcome = props => {
     const getStarted = () => {
         history.push('/collections');
     }
+
+    const handlers = useSwipeable({
+        onSwipedLeft: () => {
+            if(isWelcome) {
+                setIsWelcome(false);
+
+                document.getElementById('slider-path').setAttribute('d', "M16 4C16 1.79086 17.7909 0 20 0H36C38.2091 0 40 1.79086 40 4C40 6.20914 38.2091 8 36 8H20C17.7909 8 16 6.20914 16 4Z");
+                document.getElementById('slider-rect').classList.add('rect-translate');
+            }
+        },
+        onSwipedRight: () => {
+            if(!isWelcome) {
+                setIsWelcome(true);
+                document.getElementById('slider-path').setAttribute('d', "M0 4C0 1.79086 1.79086 0 4 0H20C22.2091 0 24 1.79086 24 4C24 6.20914 22.2091 8 20 8H4C1.79086 8 0 6.20914 0 4Z");
+                document.getElementById('slider-rect').classList.remove('rect-translate');
+            }
+        },
+        preventDefaultTouchmoveEvent: true,
+        trackMouse: true
+    });
 
     let welcome = (
         <React.Fragment>
@@ -53,7 +74,7 @@ const Welcome = props => {
     );
 
     return (
-        <div className='welcome'>
+        <div className='welcome' {...handlers}>
             <img src={isWelcome ? movieNight : watch} className="welcome-img" alt='Movie night'/>
                 <svg id='slider' width="40" height="8" viewBox="0 0 40 8" fill="none" xmlns="http://www.w3.org/2000/svg">
                     <path id='slider-path' d="M0 4C0 1.79086 1.79086 0 4 0H20C22.2091 0 24 1.79086 24 4C24 6.20914 22.2091 8 20 8H4C1.79086 8 0 6.20914 0 4Z" fill="#FCB016"/>
