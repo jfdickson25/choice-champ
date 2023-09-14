767250b83ee37e53ff49a6abb5249d07f14d25ca
Jordan Dickson <jfdickson25@gmail.com>
2023-09-13 23:14:58 -0600

    Fix routing for watched items to details, rounded providers images

--- src/collection/pages/Collection.js
+++ src/collection/pages/Collection.js
@@ -276,7 +276,7 @@ const Collection = ({ socket }) => {
                                                 // Only show if the item is watched
                                                 item.watched ?
                                                 (
-                                                    <div className='item-section' key={item.itemId} >
+                                                    <div className='item-section' key={item.itemId} onClick={ !isEdit ? () => { navDetails(item.itemId) } : null } >
                                                         <img className='item-img' src={item.poster} />{(collectionType === 'game' || collectionType === 'board') && <p>{item.title}</p>}
                                                         { isEdit ? (<img src={'https://cdn.glitch.global/7cdfb78e-767d-42ef-b9ca-2f58981eb393/remove.png?v=1682136649433'} alt={`${item.title} poster`} className='item-action' onClick={() => { removeItem(item._id) }} />) : null }
                                                         { isEdit ? (<img src={item.watched ? 'https://cdn.glitch.global/7cdfb78e-767d-42ef-b9ca-2f58981eb393/watched.png?v=1682136650141' : 'https://cdn.glitch.global/7cdfb78e-767d-42ef-b9ca-2f58981eb393/unwatched.png?v=1682136649813' } alt={`${item.title} poster`} className='item-action-watched' onClick={() => {updateWatched(item._id, item.watched)}} />) : null }
@@ -290,7 +290,7 @@ const Collection = ({ socket }) => {
                                             // Only show if the item is watched
                                             item.watched ?
                                                 (
-                                                    <div className='item-section' key={item.itemId} >
+                                                    <div className='item-section' key={item.itemId} onClick={ !isEdit ? () => { navDetails(item.itemId) } : null } >
                                                         <img className='item-img' src={item.poster} />{(collectionType === 'game' || collectionType === 'board') && <p>{item.title}</p>}
                                                         { isEdit ? (<img src={'https://cdn.glitch.global/7cdfb78e-767d-42ef-b9ca-2f58981eb393/remove.png?v=1682136649433'} alt={`${item.title} poster`} className='item-action' onClick={() => { removeItem(item._id) }} />) : null }
                                                         { isEdit ? (<img src={item.watched ? 'https://cdn.glitch.global/7cdfb78e-767d-42ef-b9ca-2f58981eb393/watched.png?v=1682136650141' : 'https://cdn.glitch.global/7cdfb78e-767d-42ef-b9ca-2f58981eb393/unwatched.png?v=1682136649813' } alt={`${item.title} poster`} className='item-action-watched' onClick={() => {updateWatched(item._id, item.watched)}} />) : null }
